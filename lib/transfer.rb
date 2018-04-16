385918f65d04884869ccf5eda3da78d29ad8c21e
Shea Kusiak <shkusiak@gmail.com>
2018-04-16 03:03:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,9 +16,10 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && sender.balance > amount
+    if self.valid? && sender.balance > amount && status == "pending"
       sender.balance -= amount
       receiver.balance += amount
+      status = "complete"
     else
       false
     end
