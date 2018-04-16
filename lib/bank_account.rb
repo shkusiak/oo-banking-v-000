efe1d5895fb7d32d99d03a5e72f33ea6e56c319d
Shea Kusiak <shkusiak@gmail.com>
2018-04-16 01:44:32 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit
+  end
+  def display_balance
+
+  end
+
+  def valid?
+  end
+
+  def close_account
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  attr_accessor :deposit, :sender, :receiver
+
+  def initialize(deposit)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @deposit = deposit
+  end
+
 end
