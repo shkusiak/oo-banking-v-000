f97b839a469c77243daea93f81524306a0243e3a
Shea Kusiak <shkusiak@gmail.com>
2018-04-16 03:12:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,9 +26,13 @@ class Transfer
 
   def reverse_transfer
     if valid? && receiver.balance > amount && @status == "complete"
-      sender.balance += amount 
+      sender.balance += amount
       receiver.balance -= amount
       @status = "reverse"
-
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
+  
 end
